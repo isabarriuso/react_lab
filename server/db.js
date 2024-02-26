ab329d8888ec532877ee161f6e847e1e19921121
isabarriuso <isabarriuso_93@hotmail.com>
2024-02-26 05:15:29 -0500

    re-did the adding of server folder

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:NjMyNy1pc2FiYXJy@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
